7122965364be9d740a7e034e79968a05468d20f7
GeraldRyan <gerald.ryan40@yahoo.com>
2020-04-17 10:24:18 -0500

    making reducer

--- smurfs/src/Store/reducers/smurfReducer.js
+++ smurfs/src/Store/reducers/smurfReducer.js
@@ -1,5 +1,44 @@
-export const initialState = {}
+export const initialState = [{
+  smurfSet: {
+    name: "Brainey",   // This level may not be necessary or even mvp as api supposed to return
+    age: "200",
+    height: "5cm",
+    id: 0
+  },
+  id: 1,
+  error: false
+}]
 
-export const smurfReducer = (state = initialState, action) =>{
-  console.log("success")
+
+const url = "http://localhost:3333/smurfs"
+
+export const smurfReducer = (state = initialState, action) =>
+{
+  switch (action.type)
+  {
+    case 'FETCH_SMURF_START':
+      return {
+
+      }
+    case 'FETCH_SMURF_SUCCESS':
+      return {
+
+      }
+    case 'FETCH_SMURF_FAIL':
+      return {
+
+      }
+    case 'POST_SMURF_START':
+      return {
+
+      }
+    case 'POST_SMURF_SUCCESS':
+      return {
+
+      }
+    case 'POST_SMURF_FAIL':
+      return {
+
+      }
+  }
 }
